92a04ee00ea5077b264c89ab309782d4fc07e2dd
skkmania <skkmania@ubu-pg84>
2010-12-03 21:40:40 +0900

    コメントとソースの見た目を修正。(refs #146)

--- shogi84/sql/get_bid.sql
+++ shogi84/sql/get_bid.sql
@@ -1,10 +1,15 @@
 -- lenny-pg の shogi2 データベースに作成した関数の覚書
 --
 --  get_bid のメイン関数
---    与えられた盤面情報を検索し、そのbidを返す。
---    同時に指し手をmovesに登録する。
---    その盤面がなければ、bidを生成し、盤面をboardsに、指し手をmovesに登録する。
---    返り値:登録したbid とmidの配列
+--    目的: ある手を指し、ある盤面ができたので、それらにmidとbidを振ってテーブルに登録してほしい。そしてそのbidとmidを教えてほしい
+--    機能: 与えられた盤面情報を検索し、
+--            既存の盤面のとき
+--              そのbidを返す。
+--              指し手は新手のはずなのでmovesに登録する。
+--            既存でないとき
+--              bidを生成し、盤面をboardsに、指し手をmovesに登録する。
+--    入力: 盤面情報と指し手情報をそれぞれの型で渡す。詳細はdeclare参照
+--    返値: 文字列 登録したbid とmidをそれぞれ文字列とし、カンマ区切りで一つの文字列として返す
 --
 create or replace function get_bid(boolean, char(81), varchar(40), varchar(40), integer, smallint, smallint, char, boolean)
   returns text as $$
@@ -24,11 +29,12 @@ create or replace function get_bid(boolean, char(81), varchar(40), varchar(40), 
        ret_str   text;
      begin
        raise notice 'get_bid : arg_turn is %  arg_board is %  arg_oldbid is %', arg_turn, arg_board, arg_oldbid;
-       select into new_bid bid from boards
-         where turn = arg_turn and
-	       black = arg_black and
-	       board = arg_board and
-	       white = arg_white;
+       SELECT into new_bid bid
+         FROM boards
+        WHERE turn = arg_turn
+          AND black = arg_black
+          AND board = arg_board
+          AND white = arg_white;
        raise notice 'get_bid : new_bid is %  arg_board is % ', new_bid, arg_board;
        if new_bid is null then
          if arg_turn then
@@ -39,7 +45,9 @@ create or replace function get_bid(boolean, char(81), varchar(40), varchar(40), 
          raise notice 'get_bid 2 : new_bid is % ', new_bid;
 	 insert into boards values(new_bid, arg_turn, arg_black, arg_board, arg_white);
        end if;
-       select coalesce(max(mid)+1,0) into new_mid from moves where bid = arg_oldbid;
+       SELECT coalesce(max(mid)+1,0) into new_mid
+         FROM moves
+        WHERE bid = arg_oldbid;
          raise notice 'get_bid 3 : new_mid is % ', new_mid;
        insert into moves values(arg_oldbid, new_mid, arg_from, arg_to, arg_piece, arg_promote, new_bid); 
        ret_str := to_char(new_bid,'999999999') || ',' || to_char(new_mid,'99');
