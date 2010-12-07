6a56c16f53913e7e7266d622bce4b35884d5381e
skkmania <skkmania@ubu-pg84>
2010-12-07 22:23:46 +0900

    getChildBidsはplpgsqlでなく、SQLで書けるのだった。(refs #166)
    PREPAREでも書けるのだが、セッションごとにしか有効ではないみたいなので関数とした。

--- shogi84/sql/getChildBidsFullSQL.sql
+++ shogi84/sql/getChildBidsFullSQL.sql
@@ -1,5 +1,5 @@
 --
--- getChildBidsFull.sql 
+-- getChildBidsFullSQL.sql 
 --    入力値:bid, level
 --    返り値:bidのテーブル
 --      bidから始め、level の深さまで、
@@ -7,20 +7,17 @@
 --      と辿って得られるbidの値を集める
 --      そうして集まった全てのbidのdistinctを返す
 --
-create or replace function getChildBidsFull( arg_bid integer, arg_level integer) returns setof integer as $$
-  begin
-    return query
-    with recursive r(bid, nxt_bid, depth) AS (
+create or replace function getChildBidsFullSQL(integer, integer) returns setof integer as $$
+  WITH RECURSIVE r(bid, nxt_bid, depth) AS (
           SELECT bid, nxt_bid, 0
             FROM moves
-           WHERE moves.bid = arg_bid
+           WHERE moves.bid = $1
         UNION ALL
           SELECT moves.bid, moves.nxt_bid, r.depth + 1
             FROM moves,r
            WHERE moves.bid = r.nxt_bid
-             AND r.depth < (arg_level - 1)
+             AND r.depth < ($2 - 1)
     )
     SELECT DISTINCT r.nxt_bid AS bid
-      FROM r;
-  end;$$
-language 'plpgsql';
+      FROM r; $$
+language SQL;
